74b03fa081e71ec55064558f4d9b4c9cf497ff7b
bnogimauemura <bnogimauemurak@icloud.com>
2025-02-20 14:00:46 +0900

    changes vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',  // Ensures that Vite listens on all network interfaces (for Render to access it)
-    port: Number(process.env.PORT) || 3000,  // Use the PORT environment variable, or default to 3000 if not set
+    port: Number(process.env.PORT) || 3000, 
+    strictPort: true, // Use the PORT environment variable, or default to 3000 if not set
   },
+  build: {
+    outDir: 'client/dist',
+  }
 });
 
